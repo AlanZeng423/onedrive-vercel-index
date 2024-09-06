a6628c25e32c27cabaad5d5526d0e6a192bac78f
AlanZeng423 <alanzeng423@gmail.com>
2024-09-06 17:45:14 +0800

    1

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/'],
+  protectedRoutes: ['/locked'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:me@alanzeng.com',
@@ -56,7 +56,7 @@ module.exports = {
       link: 'https://github.com/alanzeng423/',
     },
     {
-      name: 'My Site',
+      name: 'Chrome',
       link: 'https://alanzeng.com'
     }
   ],
